458bc1aad7981ed1db8c9c937d52c1d3a43be63f
johnmauff <dennis@ucar.edu>
2026-08-13 15:15:46 -0600

    Addresses a build error by chaning Fortran (cheating)

--- examples/opt_arg/test_opt_arg_host_integration.F90
+++ examples/opt_arg/test_opt_arg_host_integration.F90
@@ -43,7 +43,7 @@ program test_opt_arg
   end if
 
   call test_host_ccpp_physics_timestep_initial(ccpp_suite, nx, std_arg, opt_arg, opt_arg_2,       &
-    errmsg, errflg)
+    flag_for_opt_arg, errmsg, errflg)
   if (errflg /= 0) then
     write(error_unit, '(a)') "An error occurred in timestep_initial: "//trim(errmsg)
     stop 1
@@ -53,7 +53,8 @@ program test_opt_arg
   if (.not. all(std_arg == 1)) write(error_unit, '(a,3i3)') "Error: std_arg=", std_arg
   if (.not. all(opt_arg == 2)) write(error_unit, '(a,3i3)') "Error: opt_arg=", opt_arg
 
-  call test_host_ccpp_physics_run(ccpp_suite, ccpp_group, 1, nx, nx, std_arg, opt_arg, opt_arg_2, errmsg, errflg)
+  call test_host_ccpp_physics_run(ccpp_suite, ccpp_group, 1, nx, nx, std_arg, opt_arg, opt_arg_2, &
+    flag_for_opt_arg, errmsg, errflg)
   if (errflg /= 0) then
     write(error_unit, '(a)') "An error occurred in run: "//trim(errmsg)
     stop 1
@@ -64,7 +65,7 @@ program test_opt_arg
   if (.not. all(opt_arg == 3)) write(error_unit, '(a,3i3)') "Error: opt_arg=", opt_arg
 
   call test_host_ccpp_physics_timestep_final(ccpp_suite, nx, std_arg, opt_arg, opt_arg_2,         &
-    errmsg, errflg)
+    flag_for_opt_arg, errmsg, errflg)
   if (errflg /= 0) then
     write(error_unit, '(a)') "An error occurred in timestep_final: "//trim(errmsg)
     stop 1
